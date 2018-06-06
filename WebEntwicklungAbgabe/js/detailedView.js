064a33f7f3a39e61ec74e76092e8d99d97945d6b
Amin Schubhan <amin.schubhan@gmail.com>
2018-06-06 16:15:16 +0200

    alot added

--- WebEntwicklungAbgabe/js/detailedView.js
+++ WebEntwicklungAbgabe/js/detailedView.js
@@ -1,4 +1,23 @@
 function DetailedView(){
+    //TextAreas
+    this.tOrt = document.getElementById("ort");
+    this.tDatum = document.getElementById("datum");
+    //Buttons
+    this.bOpenMaps = document.getElementById("openMaps");
+    this.bNewObj = document.getElementById("newObj");
+    this.bEditObj = document.getElementById("editObj");
+    this.bDelObj = document.getElementById("delObj");
+    this.bPrintSit = document.getElementById("printSit");
+    this.bSaveSit = document.getElementById("saveSit");
+    this.bCloseSit = document.getElementById("closeSit");
+    //OnClicks
+    this.bNewObj.onclick = this.newBeObj;
+    this.bEditObj.onclick = this.editBeObj;
+    this.bDelObj.onclick = this.delBeObj;
+    this.bPrintSit.onclick = this.print;
+    this.bSaveSit.onclick = this.save;
+    this.bCloseSit.onclick = this.close;
+
     this.offeneSitzung = 0;
     this.open = function(sitzung){
         this.offeneSitzung = sitzung;
@@ -9,5 +28,6 @@ function DetailedView(){
     this.delBeObj = function(){};
     this.openLocation = function(){};
     this.close = function(){};
+    this.save = function(){};
     this.print = function(){};
 };
