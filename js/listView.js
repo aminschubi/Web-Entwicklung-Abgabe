c337b964b25aab77be8484c191e1dc89f8446344
Daniel Siladjev <danielsiladjev@web.de>
2018-06-07 18:52:07 +0200

    jo überarbeitet, damit eslint nicht heult
    sieht nach viel aus, aber meistens nur fehler wegen leerzeilen

--- js/listView.js
+++ js/listView.js
@@ -1,58 +1,57 @@
 var Sitzung = require("./sitzung");
 
-function ListView(details){
-    var this_ = this;
-    this.details = details;
-    this.details.listView = this;
-    this.sitzungen = [];
-    this.bAdd = document.getElementById("addSitzung");
-    this.bAdd.onclick = function(){this_.newSitzung();};
-    this.bNextPage = document.getElementById("nextPage");
-    this.bNextPage.onclick = function(){
-        this_.sitzungen = [];
-        this_.clean();
-    };
-    this.list = document.getElementById("sitzungen");
-    this.list.onchange = function(){
-        this_.details.closeD();
-        this_.details.open(this_.sitzungen[this_.list.selectedIndex]);
-        this_.details.bEditSit.diabled = false;
-    };
-    this.list.onclick = function(){
-        this_.details.closeD();
-        this_.details.open(this_.sitzungen[this_.list.selectedIndex]);
-        this_.details.bEditSit.diabled = false;
-    };
-};
+function ListView(details) {
+	var this_ = this;
+	this.details = details;
+	this.details.listView = this;
+	this.sitzungen = [];
+	this.bAdd = document.getElementById("addSitzung");
+	this.bAdd.onclick = function () { this_.newSitzung(); };
+	this.bNextPage = document.getElementById("nextPage");
+	this.bNextPage.onclick = function () {
+		this_.sitzungen = [];
+		this_.clean();
+	};
+	this.list = document.getElementById("sitzungen");
+	this.list.onchange = function () {
+		this_.details.closeD();
+		this_.details.open(this_.sitzungen[this_.list.selectedIndex]);
+		this_.details.bEditSit.diabled = false;
+	};
+	this.list.onclick = function () {
+		this_.details.closeD();
+		this_.details.open(this_.sitzungen[this_.list.selectedIndex]);
+		this_.details.bEditSit.diabled = false;
+	};
+}
 
-ListView.prototype.newSitzung = function(){
-    this.details.closeD();
-    var neu = new Sitzung("","",[]);
-    this.addSitzung(neu);
-    this.update();
-    this.list.selectedIndex = this.sitzungen.length-1;
+ListView.prototype.newSitzung = function () {
+	this.details.closeD();
+	var neu = new Sitzung("", "", []);
+	this.addSitzung(neu);
+	this.update();
+	this.list.selectedIndex = this.sitzungen.length - 1;
 };
-ListView.prototype.addSitzung = function(sitzung){
-    this.sitzungen.push(sitzung);
-    this.update();
+ListView.prototype.addSitzung = function (sitzung) {
+	this.sitzungen.push(sitzung);
+	this.update();
 };
-ListView.prototype.delSitzung = function(index){
-    this.sitzungen.splice(index, index);
-    this.update();
+ListView.prototype.delSitzung = function (index) {
+	this.sitzungen.splice(index, index);
+	this.update();
 };
-ListView.prototype.update = function(){
-    this.clean();
-    for(let x in this.sitzungen){
-        var newObj = document.createElement("option");
-        newObj.text = this.sitzungen[x].getDesc();
-        this.list.add(newObj);
-    }
+ListView.prototype.update = function () {
+	this.clean();
+	for (let x in this.sitzungen) {
+		var newObj = document.createElement("option");
+		newObj.text = this.sitzungen[x].getDesc();
+		this.list.add(newObj);
+	}
 };
-ListView.prototype.clean = function(){
-    for(i = this.list.options.length - 1 ; i >= 0 ; i--)
-        {
-            this.list.remove(i);
-        }
+ListView.prototype.clean = function () {
+	for (var i = this.list.options.length - 1; i >= 0; i--) {
+		this.list.remove(i);
+	}
 };
 
 module.exports = ListView;
